4a91542435774eba951f26329a3905d7b47dd9ee
jlchen5 <jl_chen1@outlook.com>
2024-04-16 16:27:02 +0200

    Update align_reads.R

--- R/align_reads.R
+++ R/align_reads.R
@@ -1,8 +1,7 @@
-align_reads <- function(fastq_path, output_path, index_path) {
-  if (!file.exists(fastq_path)) {
-    stop("FASTQ file does not exist.")
-  }
-
-  cmd <- paste("bowtie2 -x", index_path, "-U", fastq_path, "-S", output_path)
-  system(cmd, intern = FALSE)
+align_reads <- function(fastq_paths, output_path, index_path, aligner_cmd = "bowtie2", aligner_options = "") {
+  sapply(fastq_paths, function(fastq_path) {
+    cmd <- sprintf("%s -x %s -U %s -S %s %s", aligner_cmd, index_path, fastq_path, output_path, aligner_options)
+    message("Executing: ", cmd)
+    system(cmd, intern = FALSE)
+  })
 }
